0500bf08b56f379c9b9dc2a0e85db2bf58def8fe
oluwatobi oyejide <moyejide2016@gmail.com>
2020-08-07 04:55:09 +0100

    added the user profile component

---
+++ src/components/userProfile/UserProfile.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class UserProfile extends Component {
+  render() {
+    return (
+      <div>
+        <h1>PROFILE</h1>
+      </div>
+    );
+  }
+}
+
+export default UserProfile;
