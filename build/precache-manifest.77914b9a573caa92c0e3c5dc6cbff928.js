b5788225e69147e72986e14409cdfd3be18a79ec
oluwatobi oyejide <moyejide2016@gmail.com>
2020-08-11 22:05:56 +0100

    updates

---
+++ build/precache-manifest.77914b9a573caa92c0e3c5dc6cbff928.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49753f9296618bfa88da4fbbe2de57b2",
+    "url": "/ideal-store/index.html"
+  },
+  {
+    "revision": "cc6c74985c631293a775",
+    "url": "/ideal-store/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "ef64a0798065d0ea5a2d",
+    "url": "/ideal-store/static/css/main.f95d0170.chunk.css"
+  },
+  {
+    "revision": "cc6c74985c631293a775",
+    "url": "/ideal-store/static/js/2.7e52c0ce.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/ideal-store/static/js/2.7e52c0ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef64a0798065d0ea5a2d",
+    "url": "/ideal-store/static/js/main.d29735e4.chunk.js"
+  },
+  {
+    "revision": "cc98cd04097fef7f7570",
+    "url": "/ideal-store/static/js/runtime-main.5010f9c9.js"
+  }
+]);
