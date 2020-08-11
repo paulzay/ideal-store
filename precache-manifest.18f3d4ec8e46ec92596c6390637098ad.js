5501c44bcd2353b7ffb24247a14831cb65d12304
oluwatobi oyejide <moyejide2016@gmail.com>
2020-08-11 22:12:58 +0100

    Updates

---
+++ precache-manifest.18f3d4ec8e46ec92596c6390637098ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdbd6edcb4a7813c4e7e8cd047f04ab7",
+    "url": "/ideal-store/index.html"
+  },
+  {
+    "revision": "4b831beb8f1c01eca175",
+    "url": "/ideal-store/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "9e4786808d92e7885203",
+    "url": "/ideal-store/static/css/main.f95d0170.chunk.css"
+  },
+  {
+    "revision": "4b831beb8f1c01eca175",
+    "url": "/ideal-store/static/js/2.95e9ac3e.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/ideal-store/static/js/2.95e9ac3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e4786808d92e7885203",
+    "url": "/ideal-store/static/js/main.37451d41.chunk.js"
+  },
+  {
+    "revision": "cc98cd04097fef7f7570",
+    "url": "/ideal-store/static/js/runtime-main.5010f9c9.js"
+  }
+]);
